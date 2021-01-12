400ee5baa5d9f286877fc5396e075c55dcc4c150
sunkanmicode <oguntimehins@gmail.com>
2021-01-12 18:09:57 +0100

    add particles js

--- main.js
+++ main.js
@@ -95,6 +95,13 @@ const trafficCountThree = () =>{
 setInterval(trafficCountThree, 1000)
 
 
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
 
-
+particlesJS.load('particles-js', 'particles.json', function(){
+  console.log('particles.json loading ...')
+  
+  
+})
 
